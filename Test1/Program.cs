25f4cf9c4cb45e6791543d5715e245e7027ae438
Ghevi <ghevi96@gmail.com>
2020-09-18 18:40:07 +0200

    Some basic syntax

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; // for working with collections (map, filter, reduce)
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +10,61 @@ namespace Test1
     {
         static void Main(string[] args)
         {
+
+            Console.ReadLine();
         }
     }
 }
+
+// 
+
+
+// float, double, decimal 
+
+//string characterName = "John";
+//int characterAge = 35;
+
+//Console.WriteLine("There once was a man named " + characterName);
+//Console.WriteLine($"He was {characterAge} years old");
+
+//characterName = "Mike";
+
+//Console.WriteLine($"He really liked the name {characterName}");
+//Console.WriteLine($"But didn't like being {characterAge}");
+
+//string phrase = "Hello World";
+//char grade = 'C';
+//int age = 20;
+//double gpa = 3.0;
+//bool isMale = true;
+
+//string phrase = "Hello pepega" + " any truers in the chat?";
+
+//Console.WriteLine("Hello\nworld");
+//Console.WriteLine("Hello \"Dood\"");
+//Console.WriteLine(phrase);
+//Console.WriteLine(phrase.Length);
+//Console.WriteLine(phrase.ToUpper());
+//Console.WriteLine(phrase.ToLower());
+//Console.WriteLine(phrase.Contains("ello"));
+//Console.WriteLine(phrase[0]);
+//Console.WriteLine(phrase.IndexOf("pepega"));
+//Console.WriteLine(phrase.Substring(6));
+//Console.WriteLine(phrase.Substring(6, 8));
+
+//Console.WriteLine(2 + 3);
+//Console.WriteLine(6 % 3);
+//Console.WriteLine(5 + 3.2); // 5 get coerced to decimal
+//Console.WriteLine(5 / 2); // we get a int anyway
+
+//int num = 6;
+//num++;
+//num--;
+//Console.WriteLine(num);
+//Console.WriteLine(num++); // logs first then ++
+//Console.WriteLine(++num); // ++ first then logs
+//Console.WriteLine(Math.Abs(-4)); 
+//Console.WriteLine(Math.Pow(2.5, 3)); 
+//Console.WriteLine(Math.Sqrt(36)); 
+//Console.WriteLine(Math.Max(4, 90)); 
+//Console.WriteLine(Math.Round(4.3)); 
